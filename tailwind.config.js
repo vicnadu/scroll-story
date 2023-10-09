fab41c748d66c1dad5dbbdff824aadeba5b95214
Victor Nadu <victorgnadu@gmail.com>
2023-10-09 22:51:22 +0400

    Finish

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        '1': 'url("https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGRyZW5vaXItMTI0LmpwZw.jpg")',
+        '2': 'url("https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQxOTgtMDc1LXBsb3kuanBn.jpg")',
+        '3': 'url("https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcC0yNjUtcGFpNzM3Ni1jaGltLmpwZw.jpg")',
+        '4': 'url("https://images.rawpixel.com/image_1300/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGR2YW5nb2doLXRoZS1iZWRyb29tcm9iLmpwZw.jpg")',
+        '5': 'url("https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGRyZW5vaXItMTA0LmpwZw.jpg")',
+        '6': 'url("https://images.rawpixel.com/image_600/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3BkbWlzYzEtMjUtbmFwLmpwZw.jpg")'
+      }
+    },
+  },
+  plugins: [],
+}
+
